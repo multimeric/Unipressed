b18404cbd34973fe0a78ed5dae5585887bc21b43
Michael Milton <michael.r.milton@gmail.com>
2022-08-03 14:51:57 +1000

    Add tests directory

---
+++ test/test_request.py
@@ -0,0 +1,81 @@
+from unipressed import UniprotkbSearch
+from unipressed.base import Search
+
+
+def make_request(**kwargs) -> Search:
+    defaults = dict(
+        query="taxonomy_id:9606 AND length:[2500 TO *]",
+        dataset="uniprotkb",
+        format="json",
+        fields=["length"],
+    )
+    defaults.update(kwargs)
+    return Search(**defaults)  # type: ignore
+
+
+def test_search_pages():
+    import json
+
+    i = 0
+
+    for i, page in enumerate(make_request().each_page()):
+        # Test that it's valid JSON
+        json.load(page)
+
+    # Test that we get at least 500 records
+    assert i > 0
+
+
+def test_search_records_json():
+    i = 0
+
+    for i, record in enumerate(make_request(format="json").each_record()):
+        assert "primaryAccession" in record
+        assert "sequence" in record
+
+    # Test that we get many records
+    assert i > 500
+
+
+def test_search_records_tsv():
+    i = 0
+
+    for i, record in enumerate(make_request(format="tsv").each_record()):
+        assert int(record["Length"]) >= 2500
+
+    # Test that we get many records
+    assert i > 500
+
+
+def test_search_records_xml():
+    i = 0
+
+    for i, record in enumerate(make_request(format="xml").each_record()):
+        seq = record.find("{*}sequence")
+        assert int(seq.attrib["length"]) >= 2500
+
+    # Test that we get many records
+    assert i > 500
+
+
+def test_search_records_list():
+    i = 0
+
+    for i, record in enumerate(make_request(format="list").each_record()):
+        assert isinstance(record, str)
+
+    # Test that we get many records
+    assert i > 500
+
+
+def test_example():
+    """
+    Validate the readme example
+    """
+    for record in UniprotkbSearch(
+        query={"and_": [{"organelle": "chloroplast"}, {"length": (5000, "*")}]},
+        fields=["length", "gene_names"],
+    ).each_record():
+        assert isinstance(record, dict)
+        assert set(record.keys()) == {"primaryAccession", "genes", "sequence"}
+        assert record["sequence"]["length"] > 5000
---
+++ test/test_validate_fields.py
@@ -0,0 +1,43 @@
+from typing import Type, get_args, get_type_hints
+
+import pytest
+
+import unipressed.types
+from unipressed.base import Search
+
+
+@pytest.mark.parametrize(["client"], [[it] for it in unipressed.types.all_clients])
+def test_valid_query_fields(client: Type[Search]):
+    """
+    Test that all the query fields defined by our schemas are accepted by Uniprot
+    """
+    query = {}
+    query_cls = get_type_hints(client)["query"]
+    for query_field in query_cls.__optional_keys__ | query_cls.__required_keys__:
+        if query_field not in {"and_", "or_", "not_"}:
+            query[query_field] = "foo"
+    response = next(iter(client(query=query).each_response()))
+
+    # Only raise an error if the request failed, and then only if because we provided an invalid field
+    # We aren't interested in other types of validation error in this test
+    if response.status_code != 200:
+        for warning in response.json()["messages"]:
+            assert "is not a valid search field" not in warning
+
+
+@pytest.mark.parametrize(["client"], [[it] for it in unipressed.types.all_clients])
+def test_valid_return_fields(client: Type[Search]):
+    """
+    Test that all the return fields defined by our schemas are accepted by Uniprot
+    """
+    # We get a response such as "Invalid fields parameter value 'foo'" with a 400 status code if we provide an invalid field
+    fields_field = get_type_hints(client)["fields"]
+    (literal,) = get_args(fields_field)
+    response = next(
+        iter(
+            client(
+                query="ThisQueryShouldReturnNoResults", fields=get_args(literal)
+            ).each_response()
+        )
+    )
+    response.raise_for_status()
