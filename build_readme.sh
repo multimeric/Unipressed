6fe9118e82a8ccd53832bb2ab24f9243cd131578
Michael Milton <michael.r.milton@gmail.com>
2022-08-26 22:18:38 +1000

    Rerun readme with rich

--- build_readme.sh
+++ build_readme.sh
@@ -2,5 +2,5 @@ poetry run jupyter nbconvert README.ipynb \
     --to markdown \
     --TemplateExporter.exclude_input_prompt True \
     --TagRemovePreprocessor.remove_cell_tags hide-cell \
-    --TagRemovePreprocessor.remove_all_outputs_tags hide-output \
-    --execute
+    --TagRemovePreprocessor.remove_all_outputs_tags hide-output #\
+    # --execute
